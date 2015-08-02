526df06c2d817b6b8732051f2119988141c5c3f0
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-02 07:47:13 -0400

    Add comments to CRUD operations

--- chapter03/song.rb
+++ chapter03/song.rb
@@ -18,37 +18,44 @@ end
 
 DataMapper.finalize
 
+# Show all songs
 get '/songs' do
   @songs = Song.all
   slim :songs
 end
 
-get '/songs/new' do
-  @song = Song.new
-  slim :new_song
-end
-
+# Show one song
 get '/songs/:id' do
   @song = Song.get(params[:id])
   slim :show_song
 end
 
+# Create a song form
+get '/songs/new' do
+  @song = Song.new
+  slim :new_song
+end
+
+# Create a song POST action
 post '/songs' do
   song = Song.create(params[:song])
   redirect to("/songs/#{song.id}")
 end
 
+# Edit a song form
 get '/songs/:id/edit' do
   @song = Song.get(params[:id])
   slim :edit_song
 end
 
+# Edit a song PUT action
 put '/songs/:id' do
   song = Song.get(params[:id])
   song.update(params[:song])
   redirect to("/songs/#{song.id}")
 end
 
+# Delete a song DELETE action
 delete '/songs/:id' do
   Song.get(params[:id]).destroy
   redirect to('/songs')
