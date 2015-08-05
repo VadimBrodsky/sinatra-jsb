7ce8022251657de6402964da1bfab6d81f2231ce
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-05 07:44:34 -0400

    Start Chapter 7

---
+++ chapter07/main.rb
@@ -0,0 +1,132 @@
+require 'sinatra'
+require './sinatra/auth'   # custom authorization extension
+require 'sinatra/reloader' if settings.development?
+require 'sinatra/flash'
+require 'slim'
+require 'pony'
+require 'coffee-script'
+
+require './song'
+
+configure do
+	set :public_folder, 'assets' # public by default
+	set :views, 'templates'      # views by default
+	enable :sessions
+	set :username, 'admin'
+	set :password, 'password'
+end
+
+helpers do
+	def css(*stylesheets)
+		stylesheets.map do |stylsheet|
+			"<link href=\"#{stylsheet}.css\" media=\"screen, projection\" rel=\"stylsheet\" />"
+		end.join
+	end
+
+	def current?(path='/')
+		(request.path == path || request.path == path + '/') ? "current" : nil
+	end
+
+	def set_title
+		@title ||= "Songs By Sinatra"
+	end
+
+	def send_message
+		Pony.mail(
+		:from => params[:name] + "<" + params[:email] + ">",
+		:to => 'email@gmail.com',
+		:subject => params[:name] + " has contacted you",
+		:body => params[:message],
+		:via => :smtp,
+		:via_options => {
+			:address              => 'smtp.gmail.com',
+			:port                 => '587',
+			:enable_starttls_auto => true,
+			:user_name            => 'email@gmail.com',
+			:password             => 'super_duper_secret',
+			:authentication       => :plain,
+			:domain               => 'localhost.localdomain'
+		})
+	end
+end
+
+# before filter - will run before each request
+# there are also after filters in sinatra
+# can be applied globally or to a specific route
+before do
+	set_title
+end
+
+after '/special' do
+	# something that happens only after the special route was invoked
+end
+
+get ('/styles.css'){ scss :styles }
+get ('/javascripts/application.js') { coffee :application }
+
+get '/' do
+	slim :home
+end
+
+get '/about' do
+	@title = "All About This Website"
+	slim :about
+end
+
+get '/contact' do
+	slim :contact
+	#slim :contact, :layout => :special
+end
+
+post '/contact' do
+	send_message
+	flash[:notice] = "Thank you for your message. We'll be in touch soon."
+	redirect to('/')
+end
+
+not_found do
+	slim :not_found
+end
+
+get '/fake-error' do
+	status 500
+	"There's nothing wrong, really :p"
+end
+
+get '/environment' do
+	if settings.development?
+		"development"
+	elsif settings.production?
+		"production"
+	elsif settings.test?
+		"test"
+	else
+		"Something else?"
+	end
+end
+
+get '/set/:name' do
+	session[:name] = params[:name]
+end
+
+get '/get/hello' do
+	"Hello #{session[:name]}"
+end
+
+get '/login' do
+	slim :login
+end
+
+# post '/login' do
+# 	if params[:username] == settings.username && params[:password] == settings.password
+# 		session[:admin] = true
+# 		redirect to('/songs')
+# 	else
+# 		slim :login
+# 	end
+# end
+
+# get '/logout' do
+# 	session.clear
+# 	redirect to('/login')
+# end
---
+++ chapter07/song.rb
@@ -0,0 +1,99 @@
+require 'dm-core'
+require 'dm-migrations'
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/development.db")
+end
+
+class Song
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :lyrics, Text
+  property :length, Integer
+  property :released_on, Date
+  property :likes, Integer, :default => 0
+
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
+end
+
+DataMapper.finalize
+
+module SongHelpers
+  def find_songs
+    @songs = Song.all
+  end
+
+  def find_song
+    Song.get(params[:id])
+  end
+
+  def create_song
+    @song = Song.create(params[:song])
+  end
+end
+
+helpers SongHelpers
+
+# Show all songs
+get '/songs' do
+  # @songs = Song.all
+  find_songs
+  slim :songs
+end
+
+# Create a song form
+get '/songs/new' do
+  protected!
+  @song = Song.new
+  slim :new_song
+end
+
+# Show one song
+get '/songs/:id' do
+  unless Song.get(params[:id]).nil?
+    @song = find_song
+    slim :show_song
+  else
+    redirect to('/songs')
+  end
+end
+
+# Edit a song form
+get '/songs/:id/edit' do
+  @song = find_song
+  slim :edit_song
+end
+
+# Create a song POST action
+post '/songs' do
+  flash[:notice] = "Song successfully added" if create_song
+  redirect to("/songs/#{@song.id}")
+end
+
+# Edit a song PUT action
+put '/songs/:id' do
+  song = find_song
+  if song.update(params[:song])
+    flash[:notice] = 'Song successfully updated'
+  end
+  redirect to("/songs/#{song.id}")
+end
+
+# Delete a song DELETE action
+delete '/songs/:id' do
+  if find_song.destroy
+    flash[:notice] = "Song deleted"
+  end
+  redirect to('/songs')
+end
+
+post '/songs/:id/like' do
+  @song = find_song
+  @song.likes = @song.likes.next
+  @song.save
+  redirect to "/songs/#{@song.id}" unless request.xhr?
+  slim :like, :layout => false
+end
